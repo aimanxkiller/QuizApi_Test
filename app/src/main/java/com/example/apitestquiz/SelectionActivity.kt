0b81b56cba4d41913152a5e9b97c3097f1f4bd1e
aimanxkiller <aimanxkiller@gmail.com>
2023-02-02 11:36:20 +0800

    New Logging - Logging with okHTTP - Using retrofit onFailure to see network connection issue - URL Manipulation - Added selection activity so user can select quiz

--- app/src/main/java/com/example/apitestquiz/SelectionActivity.kt
+++ app/src/main/java/com/example/apitestquiz/SelectionActivity.kt
@@ -1,20 +1,20 @@
 package com.example.apitestquiz
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SelectionActivity : AppCompatActivity() {
     private lateinit var spinner:Spinner
-    lateinit var buttonNext:Button
+    private lateinit var buttonNext:Button
     lateinit var selection:String
 
+    private var categoryName = arrayOf("Science","Arts and Literature","Film and Tv","Food and Drink","General Knowledge","Geography","History","Sports and Leisure","Society and Culture","Music")
     private var category = arrayOf("science","arts_and_literature","film_and_tv","food_and_drink","general_knowledge","geography","history","sport_and_leisure","society_and_culture","music")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +23,7 @@ class SelectionActivity : AppCompatActivity() {
 
         buttonNext = findViewById(R.id.buttonSel)
         spinner = findViewById(R.id.dropdownList)
-        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,category)
+        val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,categoryName)
         spinner.adapter = arrayAdapter
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
