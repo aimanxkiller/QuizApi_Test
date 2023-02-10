9d2f3fe1cfb52cfd05ab562c35ade51c7ed6d1ce
aimanxkiller <aimanxkiller@gmail.com>
2023-02-10 09:07:47 +0800

    New Logging - Logging with okHTTP - Using retrofit onFailure to see network connection issue - URL Manipulation - Added selection activity so user can select quiz - Updated selection from static to api calling - Updated using coroutine for error handling and api calling - Added ViewPager view - - Added next previous dynamic button and finish button on final page

--- app/src/main/java/com/example/apitestquiz/viewmodel/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/apitestquiz/viewmodel/ViewPagerAdapter.kt
@@ -2,7 +2,6 @@ package com.example.apitestquiz.viewmodel
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -37,7 +36,7 @@ class ViewPagerAdapter(private var list:List<QuestionModelItem>):RecyclerView.Ad
     }
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewPagerAdapter.Pager2ViewHolder, position:Int){
-        holder.textView.text = list[position].question
+        holder.textView.text = (list[position].question)
         val answers = getAnswerCollection(list,position)
 
         holder.radioGroup.children.forEachIndexed { index, view ->
@@ -60,21 +59,24 @@ class ViewPagerAdapter(private var list:List<QuestionModelItem>):RecyclerView.Ad
             }
         }
 
+        holder.buttonRight.setOnClickListener {
+            (holder.itemView.parent as RecyclerView).smoothScrollToPosition(position + 1)
+        }
+        holder.buttonLeft.setOnClickListener {
+            (holder.itemView.parent as RecyclerView).smoothScrollToPosition(position - 1)
+        }
+        holder.buttonRight.text = "Next"
+        holder.buttonLeft.text = "Previous"
+
         when(position){
             0-> {
                 holder.buttonLeft.visibility = View.INVISIBLE
                 holder.buttonRight.visibility = View.VISIBLE
-                holder.buttonRight.text = "Next"
-                holder.buttonRight.setOnClickListener {
-                    Log.e("Debug","ButtonClicked")
-                    (holder.itemView.parent as RecyclerView).smoothScrollToPosition(position + 1)
-                }
             }
             4 ->{
                 holder.buttonLeft.visibility = View.VISIBLE
                 holder.buttonRight.visibility = View.VISIBLE
                 holder.buttonRight.text = "Finish"
-                holder.buttonLeft.text = "Previous"
 
                 holder.buttonRight.setOnClickListener {
                     val context = holder.itemView.context
@@ -84,13 +86,10 @@ class ViewPagerAdapter(private var list:List<QuestionModelItem>):RecyclerView.Ad
                         context.startActivity(intent)
                     }else{ Toast.makeText(context,"Answer All Question",Toast.LENGTH_SHORT).show() }
                 }
-
             }
             else -> {
                 holder.buttonLeft.visibility = View.VISIBLE
                 holder.buttonRight.visibility = View.VISIBLE
-                holder.buttonRight.text = "Next"
-                holder.buttonLeft.text = "Previous"
             }
         }
 
